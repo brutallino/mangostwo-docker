e499c7f29b337c56e6f35af23977540d34210d88
ilix <me@ilix.se>
2015-02-09 20:52:03 +0100

    Added shared install-mangos.sh to contain common compilation instructions

--- world/install.sh
+++ world/install.sh
@@ -1,17 +1,3 @@
 #!/bin/sh
 
-mkdir mangos
-cd mangos
-
-git clone --recursive http://github.com/mangostwo/server.git
-
-cd server
-
-mkdir objdir
-cd objdir
-
-cmake .. -DCMAKE_INSTALL_PREFIX=/opt/mangos-server
-make
-make install
-
 chmod +x /opt/mangos-server/bin/mangosd
