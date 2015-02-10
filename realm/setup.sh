7edbea67bfbbb38d012629addfcb558399997573
ilix <me@ilix.se>
2015-02-10 17:06:05 +0100

    Modified scripts, location of config.

--- realm/setup.sh
+++ realm/setup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+mkdir mangos
+cd mangos
+
+git clone --recursive http://github.com/mangostwo/server.git
+
+cd server
+
+mkdir objdir
+cd objdir
+
+cmake .. -DCMAKE_INSTALL_PREFIX=/opt/mangos-server -DACE_USE_EXTERNAL=1
+make
+make install
+
 chmod +x /opt/mangos-server/bin/realmd
-cp /tmp/realmd.conf /opt/mangos-server/bin/realmd.conf
+cp /tmp/realmd.conf /opt/mangos-server/etc/realmd.conf
